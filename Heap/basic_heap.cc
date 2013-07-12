aaeb64b4c29efdf30c79d0a0a624be91c7a38454
Mukil Krishnan <mike@Spitfire.(none)>
2013-07-11 20:00:36 -0500

    added heap_sort() function

--- Heap/basic_heap.cc
+++ Heap/basic_heap.cc
@@ -2,6 +2,10 @@
 #include<vector>
 #include<cstdlib>
 
+/*
+Function that maintains the heap property
+Allows element at index to float down the heap into position
+*/
 template<typename T>
 void max_heapify(std::vector<T>& A, int const& index, int const& heapsize)
 {
@@ -23,6 +27,9 @@ void max_heapify(std::vector<T>& A, int const& index, int const& heapsize)
 	}
 }
 
+/*
+Function that builds a max heap from the elements in vector A
+*/
 template<typename T>
 void build_max_heap(std::vector<T>& A)
 {
@@ -30,6 +37,23 @@ void build_max_heap(std::vector<T>& A)
 	for(int i = (heapsize - 1)/2; i >= 0; --i)
 		max_heapify(A, i, heapsize);
 }
+
+/*
+Function that sorts the elements in vector A in place
+*/
+template<typename T>
+void heap_sort(std::vector<T>& A)
+{
+	build_max_heap(A);
+	int heapsize = A.size();
+	for(int i = A.size() - 1; i >= 1; --i)
+	{
+		std::swap(A[0], A[i]);
+		heapsize--;
+		max_heapify(A, 0, heapsize);
+	}
+}
+
 
 int main()
 {
@@ -42,6 +66,7 @@ int main()
 	std::cout << std::endl;
 	
 	build_max_heap(A);
+	heap_sort(A);
 	
 	for(std::vector<int>::iterator it = A.begin(); it != A.end(); ++it)
 		std::cout << *it << " ";
